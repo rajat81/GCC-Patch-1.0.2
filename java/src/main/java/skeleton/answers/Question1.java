6f6cef67ecde909b601861187d9b97353629eb08
Rajat Sawant <rajatswnt@gmail.com>
2019-10-07 11:07:07 +0530

    Update 1

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -1,10 +1,40 @@
 package skeleton.answers;
+import java.util.*;
+public class Question2 {
 
-public class Question1 {
+    public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
+        int finalProfit = 0;
+        for (int i = 0; i < trader.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
 
-    public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
-        // TODO Auto-generated method stub
-        return -5;
+            for (int j = 0; j < risk.length; j++) {
+                if (trader[i]>= risk[j]) {
+                    list.add(bonus[j]);
+                }
+            }
+
+            finalProfit += Collections.max(list);
+        }
+
+        return finalProfit;
+       // return -5;
     }
 
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] trader = new int[n];
+//        int[] risk = new int[n];
+//        int[] bonus = new int[n];
+//
+//        for (int i = 0; i < n ; i++)
+//            trader[i] = sc.nextInt();
+//
+//        for (int i = 0; i < n ; i++)
+//            risk[i] = sc.nextInt();
+//
+//        for (int i = 0; i < n ; i++)
+//            bonus[i] = sc.nextInt();
+//    }
 }
+
--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -1,9 +1,39 @@
 package skeleton.answers;
-
+import java.util.*;
 public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
-        // TODO Auto-generated method stub
-        return -5;
+        int finalProfit = 0;
+        for (int i = 0; i < trader.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int j = 0; j < risk.length; j++) {
+                if (trader[i]>= risk[j]) {
+                    list.add(bonus[j]);
+                }
+            }
+
+            finalProfit += Collections.max(list);
+        }
+
+        return finalProfit;
+       // return -5;
     }
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] trader = new int[n];
+//        int[] risk = new int[n];
+//        int[] bonus = new int[n];
+//
+//        for (int i = 0; i < n ; i++)
+//            trader[i] = sc.nextInt();
+//
+//        for (int i = 0; i < n ; i++)
+//            risk[i] = sc.nextInt();
+//
+//        for (int i = 0; i < n ; i++)
+//            bonus[i] = sc.nextInt();
+//    }
 }
